81b78d7d057fc8755ff917833f2f0c951cb131e2
hamisicodes <newhamisi@gmail.com>
2020-05-29 13:02:54 +0300

    define a login method in the views module

--- insta/views.py
+++ insta/views.py
@@ -1,7 +1,36 @@
 from django.shortcuts import render
-from django.http import HttpRequest
+from django.http import HttpRequest,HttpResponse
+from .forms import loginForm
+from django.contrib.auth import authenticate,login
 
 # Create your views here.
 
 def home(request):
     return render(request, 'home.html')
+
+def user_login(request):
+    if request.method == 'POST':
+        form = loginForm(request.POST):
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request,
+                                username =  cd['username'],
+                                password =  cd['password']
+                                 )    
+            if user is not None:
+                if user.is_active():
+                    login(request ,user)
+                    return HttpResponse('Authenticated Successfully')
+
+                else:
+                    return HttpResponse('Account disabled')
+
+            
+            else:
+                return HttpResponse('Invalid Login')
+
+
+    else:
+        form = loginForm()
+    
+    return render(request , account/login.html , {'form':form})
