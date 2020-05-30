bc14e68c806927c0027fa71d18f428b8e7e61944
hamisicodes <newhamisi@gmail.com>
2020-05-30 13:41:32 +0300

    Add media settings

--- insta/models.py
+++ insta/models.py
@@ -4,7 +4,7 @@ from django.conf import settings
 
 # Create your models here.
 class  Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE),
+    user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE), #To refer the user model
     date_of_birth = models.DateField(blank =True, null =True),
     photo = models.ImageField(upload_to='users/' , blank=True)
 
