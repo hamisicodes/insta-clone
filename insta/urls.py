126db67c05370d637494d35218a2bf6302e3e174
hamisicodes <newhamisi@gmail.com>
2020-05-31 17:24:16 +0300

    Add url path for the create view function

--- insta/urls.py
+++ insta/urls.py
@@ -10,7 +10,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/' , auth_views.LoginView.as_view() ,name ='login'),
     path('logout/' , auth_views.LogoutView.as_view(),{"next_page": '/'} ,name ='logout' ),
-    path('edit/', views.edit , name='edit')
+    path('edit/', views.edit , name='edit'),
+    path('create/', views.create , name='create')
 ]
 
 if settings.DEBUG:
