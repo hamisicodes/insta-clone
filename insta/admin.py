85845159b666bfe80c052b03a1c7690dfbb32f1b
hamisicodes <newhamisi@gmail.com>
2020-05-31 13:25:52 +0300

    register the Image model into the asdmin site

--- insta/admin.py
+++ insta/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
-from .models import Profile
+from .models import Profile,Image
 
 # Register your models here.
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('user','date_of_birth', 'photo','bio')
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display =('image','name','caption')
 
